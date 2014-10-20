f5778f11f802d5ed4cd171859a0698e197dab8d7
Justin Johnson <jjohnson@openroad.ca>
2014-10-19 23:42:56 -0700

    Whoops

---
+++ level.js
@@ -0,0 +1,248 @@
+function Level(options) {
+    this.width = options.width;
+    this.height = options.height;
+    this.level = options.level;
+    this.start = options.start;
+    this.element = document.createElement("div");
+    this.grid = [];
+    this.available = {};
+    this.open = [];
+    this.mobs = [];
+    this.items = [];
+
+    var xx, yy, row;
+    for (yy = 0; yy < this.height; ++yy) {
+        row = document.createElement("div");
+        row.className = "row";
+        for (xx = 0; xx < this.width; ++xx) {
+            if (! this.grid[xx]) { this.grid[xx] = []; }
+            this.grid[xx].push(new Square({x: xx, y: yy, level: this}));
+            row.appendChild(this.grid[xx][yy].span());
+        }
+        this.element.appendChild(row);
+    }
+    // Give each square a cardinal direction reference to its neighbours
+    for (xx = 0; xx < this.width; ++xx) {
+        for (yy = 0; yy < this.height; ++yy) {
+            for (var dir in Square.prototype.cardinals) {
+                try {
+                    var nx = xx + Square.prototype.cardinals[dir][0],
+                        ny = yy + Square.prototype.cardinals[dir][1];
+                    this.grid[xx][yy][dir] = this.grid[nx][ny];
+                } catch(err) { this.grid[xx][yy][dir] = undefined; }
+            }
+        }
+    }
+}
+
+/**
+ */
+Level.prototype.random = function(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+};
+
+Level.prototype.choice = function() {
+    return arguments[this.random(0, arguments.length)];
+};
+
+
+Level.prototype.getRoomParameters = function() {
+    var direction = this.choice("n", "w", "e", "s"),
+        a = {min: this.random(2, 3), max: this.random(3, 8)},
+        b = {min: this.random(2, 4), max: this.random(4, 12)};
+    if (direction === "n" || direction === "s") {
+        return {dir: direction, lat: b, lng: a};
+    }
+    return {dir: direction, lat: a, lng: b};
+};
+
+Level.prototype.generateLevel = function() {
+    this.entry = this.grid[this.start.x][this.start.y];
+    var params = this.getRoomParameters(),
+        max_attempts = 60,
+        max_rooms = 15,
+        num_rooms = 1,
+        max_mobs = 15,
+        max_items = 3,
+        mob, item, loc, xy;
+    this.addRoom(this.entry, params.dir, params.lat, params.lng);
+    for (var ii = 0; ii < max_attempts && num_rooms < max_rooms; ++ii) {
+        var keys = Object.keys(this.available),
+            available = this.available[keys.choice()],
+            door = available.wall,
+            dir = available.dir;
+        params = this.getRoomParameters();
+        if (this.addRoom(door, dir, params.lat, params.lng)) { ++num_rooms; }
+    }
+
+    // add entry and exit
+    this.entry = this.grid[this.entry.x][this.entry.y];
+    this.entry.add(up);
+    this.open.remove(this.entry.id());
+    this.exit = this.choice.apply(this, this.open).split(",");
+    this.exit = this.grid[this.exit[0]][this.exit[1]];
+    this.exit.add(down);
+
+    // add mobs
+    for (var kk = 0; kk < max_mobs; ++kk) {
+        mob = this.generateMob();
+        xy = this.open.draw().split(",");
+        this.mobs.push(mob);
+        this.grid[xy[0]][xy[1]].add(mob);
+    }
+
+    // add items
+    for (var ll = 0; ll < max_items; ++ll) {
+        item = this.generateItem([items.chest, items.pile].choice());
+        xy = this.open.draw().split(",");
+        this.items.push(item);
+        this.grid[xy[0]][xy[1]].add(item);
+    }
+};
+
+Level.prototype.generateMob = function() {
+    var type = Object.keys(mobs).choice(),
+        mob = Object.create(mobs[type]);
+    mob.hp = 10;
+    mob.classes.push(type);
+    return mob;
+};
+
+Level.prototype.generateItem = function() {
+    var type = arguments[0] || items[Object.keys(items).choice()],
+        item = Object.create(type);
+    return item;
+};
+
+Level.prototype.addRoom = function(door, dir, lat, lng) {
+    /**
+     * The meat of dungeon generation: this algorithm finds an available wall,
+     * adds a door to it, then tries to carve out a room at least min height
+     * and width and at most max height and width.  The direction in which the
+     * carver is facing (i.e., in which wall of the existing room the door is)
+     * is the longitudinal axis of the new room (lng* variables, below; lat*
+     * are lateral equivalents).  The carver digs laterally to max_lat,
+     * randomly alternating between left and right for the first row, and then
+     * advances along the longitudinal axis and and carves laterally to the
+     * left/right boundaries established in the first row.
+     *
+     * If at any time, the carver cannot continue because the possible square
+     * doesn't have wall in the next square laterally and longitudinally, room
+     * generation stops and min_* values are checked.  If the room is big
+     * enough, it's committed; otherwise it's abandoned.
+     */
+
+    var lat_choices = (dir === "n" || dir === "s" ? ["w", "e"] : ["n", "s"]),
+        choices = lat_choices.slice(),
+        lng_choices = (dir === "n" || dir === "s" ? ["n", "s"] : ["w", "e"]),
+        lng_axis = (dir === "n" || dir === "s" ? "y" : "x"),
+        lat_axis = (dir === "n" || dir === "s" ? "x" : "y"),
+        dz = (dir === "n" || dir === "w" ? -1 : 1),
+        rdir = Square.prototype.reverse[dir],
+        ldir = lat_choices[Number(dz === 1)],
+        nw = dir + ldir,
+        se = dir + lat_choices[Number(dz === -1)],
+        room = {};
+    for (ii = 0; ii < lat_choices.length; ++ii) {
+        room[lat_choices[ii]] = door[dir];
+        room[lng_choices[ii]] = door[dir];
+        room[nw] = door[dir];
+        room[se] = door[dir];
+    }
+
+    for (ii = 0; ii < lat.max - 1 && choices.length > 0; ++ii) {
+        var cdir = choices.choice(),
+            latp = room[cdir][cdir],
+            cpdir = ["n", "s"].indexOf(dir) > -1 ? dir + cdir : cdir + dir;
+        if (latp.isOkForRoom(dir, cdir, rdir, cpdir)) {
+            room[cdir] = latp;
+            room[dir + cdir] = latp;
+        } else {
+            if ((ii = choices.indexOf(cdir)) > -1) { choices.splice(ii, 1); }
+        }
+    }
+    var width = room[lat_choices[1]][lat_axis] - room[lat_choices[0]][lat_axis];
+    if (width + 1 < lat.min) { return false; }
+    loop: for (kk = 1; kk < lng.max; ++kk) {
+        var cdir = lat_choices[1],   // jshint ignore:line
+            dirs = [dir, cdir];
+        if (room[lat_choices[0]][dir].isOkForRoom(dir, cdir, lat_choices[0])) {
+            if (["n", "s"].indexOf(dir) > -1) {
+                dirs.push(dir + lat_choices[0]);
+                dirs.push(dir + lat_choices[1]);
+            } else {
+                dirs.push(lat_choices[0] + dir);
+                dirs.push(lat_choices[1] + dir);
+            }
+            var xdir = ["n", "s"].indexOf(dir) > -1 ? dir + cdir : cdir + dir;
+            room[cdir] = room[lat_choices[0]] = room[lat_choices[0]][dir];
+            for (ii = 0; ii < width; ++ii) {
+                if (! room[cdir][cdir].isOkForRoom.apply(dirs)) {
+                    break loop;
+                }
+                room[cdir] = room[cdir][cdir];
+            }
+            room[nw] = room[ldir];
+        }
+    }
+    if (room[nw][lat_axis] > room[se][lat_axis]) {
+        room.tmp = room[nw];
+        room[nw] = room[se];
+        room[se] = room.tmp;
+    }
+    var height = room[se][lng_axis] - room[nw][lng_axis];
+    if (height + 1 < lng.min) { return false; }
+    for (xx = room[nw].x; xx <= room[se].x; ++xx) {
+        for (yy = room[nw].y; yy <= room[se].y; ++yy) {
+            this.grid[xx][yy].add(floor);
+            this.open.push(xx + "," + yy);
+        }
+    }
+    this.grid[door.x][door.y].add(floor);
+
+    // update available walls
+    for (xx = room[nw].x; xx <= room[se].x; ++xx) {
+        this.grid[xx][room[nw].y].markAvailableWalls("n");
+        this.grid[xx][room[se].y].markAvailableWalls("s");
+    }
+    for (yy = room[nw].y; yy <= room[se].y; ++yy) {
+        this.grid[room[nw].x][yy].markAvailableWalls("w");
+        this.grid[room[se].x][yy].markAvailableWalls("e");
+    }
+
+    return true;
+};
+
+Level.prototype.isAllowed = function(position) {
+    if (position.x < 0 || position.x >= this.width) { return false; }
+    if (position.y < 0 || position.y >= this.height) { return false; }
+    return position.entity !== wall;
+};
+
+
+var visibility_grid = [
+    [[1, 2], [2], [1, 2]],
+    [[2], [], [2]],
+    [[1, 2], [2], [1, 2]],
+];
+Level.prototype.updateVisibility = function(square) {
+    var x = square.x, y = square.y;
+    for (var yy = -1; yy <= 1; ++yy) {
+        for (var xx = -1; xx <= 1; ++xx) {
+            if (! this.grid[x + xx]) { continue; }
+            if (! this.grid[x + xx][y + yy]) { continue; }
+            this.grid[x + xx][y + yy].updateVisibility(3);
+            if (this.grid[x + xx][y + yy].entity != wall) {
+                var xtd = visibility_grid[xx + 1][yy + 1];
+                this.grid[x + xx + xx][y + yy + yy].updateVisibility(xtd[0]);
+                if (xtd.length > 1) {
+                    this.grid[x + xx + xx][y + yy].updateVisibility(xtd[1]);
+                    this.grid[x + xx][y + yy + yy].updateVisibility(xtd[1]);
+                }
+            }
+        }
+    }
+};
+
+
+
---
+++ square.js
@@ -0,0 +1,121 @@
+function Square(options) {
+    this.x = options.x;
+    this.y = options.y;
+    this.visibility = 3;
+    this.entity = options.entity || wall;
+    this.level = options.level;
+    this._span = undefined;
+    this._classes = ['grid'].concat(this.entity.classes || []);
+    this._previous = [];
+}
+
+Square.prototype.add = function(entity) {
+    this._previous.push(this.entity);
+    this.entity.classes.forEach(function(cls) { this.removeClass(cls); }, this);
+    this.entity = entity;
+    this.entity.classes.forEach(function(cls) { this.addClass(cls); }, this);
+    this.span().innerHTML = entity.html;
+};
+
+Square.prototype.remove = function(entity) {
+    this.entity.classes.forEach(function(cls) { this.removeClass(cls); }, this);
+    this.entity = this._previous.pop();
+    this.entity.classes.forEach(function(cls) { this.addClass(cls); }, this);
+    this.span().innerHTML = this.entity.html;
+};
+
+Square.prototype.cardinals = {"nw": [-1, -1], "n": [0, -1], "ne": [1, -1],
+                              "e": [1, 0], "se": [1, 1], "s": [0, 1],
+                              "sw": [-1, 1], "w": [-1, 0]};
+Square.prototype.reverse = {"nw": "se", "n": "s", "ne": "sw", "e": "w",
+                            "se": "nw", "s": "n", "sw": "ne", "w": "e"};
+Square.prototype.lighting = ["pitch", "dark", "dim", "visible"];
+
+Square.prototype.span = function() {
+    if (this._span === undefined) {
+        this._span = document.createElement("span");
+        this._span.setAttribute("id", this.x + "," + this.y);
+        this._span.innerHTML = this.entity.html;
+        this._classes.push(this.lighting[this.visibility]);
+        this._span.className = this._classes.join(" ");
+    }
+    return this._span;
+};
+
+Square.prototype.updateVisibility = function(level) {
+    if (level > this.visibility) {
+        this.removeClass(this.lighting[this.visibility]);
+        this.visibility = level;
+        this.addClass(this.lighting[this.visibility]);
+    }
+};
+
+Square.prototype.addClass = function(cls) {
+    if (this._classes.indexOf(cls) === -1) {
+        this._classes.push(cls);
+        this.span().className = this._classes.join(" ");
+    }
+};
+
+Square.prototype.removeClass = function(cls) {
+    if ((ii = this._classes.indexOf(cls)) > -1) {
+        this._classes.splice(ii, 1);
+        this.span().className = this._classes.join(" ");
+    }
+};
+
+Square.prototype.getOpenAdjacent = function() {
+    var adj = [];
+    if (this.n && this.n.entity != wall) {
+        adj.push({x: this.n.x, y: this.n.y, dir: "n"});
+    }
+    if (this.e && this.e.entity != wall) {
+        adj.push({x: this.e.x, y: this.e.y, dir: "e"});
+    }
+    if (this.s && this.s.entity != wall) {
+        adj.push({x: this.s.x, y: this.s.y, dir: "s"});
+    }
+    if (this.w && this.w.entity != wall) {
+        adj.push({x: this.w.x, y: this.w.y, dir: "w"});
+    }
+    return adj;
+};
+
+Square.prototype.isOkForRoom = function() {
+    for (var ii = 0; ii < arguments.length; ++ii) {
+        if (! this[arguments[ii]] || this[arguments[ii]].entity !== wall) {
+            return false;
+        }
+    }
+    return true;
+};
+
+Square.prototype.markAvailableWalls = function(dir) {
+    var contender = this[dir],
+        cursor = this[dir],
+        id = contender.span().getAttribute("id"),
+        dz, l, r;
+    if (dir === "n") { dz = -1; l = "w"; r = "e"; }
+    else if (dir === "s") { dz = 1; l = "e"; r = "w"; }
+    else if (dir === "w") { dz = -1; l = "s"; r = "n"; }
+    else { dz = 1; l = "n"; r = "s"; }
+    for (var ii = 0; ii < 3 && contender && cursor; ++ii) {
+        if (! (cursor && cursor.entity === wall &&
+               cursor[l] && cursor[l].entity === wall &&
+               cursor[r] && cursor[r].entity === wall &&
+               cursor[dir])) {
+            //contender.removeClass("available");
+            delete this.level.available[id];
+            return false;
+        }
+        cursor = cursor[dir];
+    }
+    this.level.available[id] = {wall: contender, dir: dir};
+    //contender.addClass("available");
+    return true;
+};
+
+Square.prototype.id = function() { return this.x + "," + this.y; };
+
+
+
